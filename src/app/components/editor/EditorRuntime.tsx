e4adbc63e8da4e9b7d92fdeefa8e8b7b7dcf28b7
Jordy Verveer <jordyverveer@gmail.com>
2026-06-25 20:58:02 +0200

    init

---
+++ src/app/components/editor/EditorRuntime.tsx
@@ -0,0 +1,3 @@
+export function EditorRuntime({ children }: { children?: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ src/app/components/game/GameRuntime.tsx
@@ -0,0 +1,3 @@
+export function GameRuntime({ children }: { children?: React.ReactNode }) {
+  return <>{children}</>;
+}
